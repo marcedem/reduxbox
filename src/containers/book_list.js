43d9b258afab8d8c500fd4cc9e25672f6abaca78
marcedem <marcsea2000@gmail.com>
2017-05-10 03:53:25 +0200

    bindActionCreators typo corrected

--- src/containers/book_list.js
+++ src/containers/book_list.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import {connect} from 'react-redux';
-import {selectBook} from '../actions/index';
-import {bindActionReactions} from 'redux';
+import { connect } from 'react-redux';
+import { selectBook } from '../actions/index';
+import { bindActionCreators } from 'redux';
 
 class BookList extends Component {
 
@@ -9,7 +9,12 @@ class BookList extends Component {
         // map over array of books
         return this.props.books.map((book) => {
             return (
-                <li key={book.title} className="list-group-item">{book.title}</li>
+                <li 
+                    key={book.title} 
+                    onClick={() => this.props.selectBook(book)}
+                    className="list-group-item">
+                    {book.title}
+                </li>
             );
         });
     }
@@ -36,7 +41,7 @@ function mapStateToProps(state){
 // Any returned from this function will ends up as props on the BookList container
 function mapDispatchToPros(dispatch){
     // Whenever selectBook is called, the result should be passed to all of our reducers
-    return bindActionReactions({selectBook: selectBook}, dispatch);
+    return bindActionCreators({selectBook: selectBook}, dispatch);
 }
 
 // Promote BookList from a component to a container - 
